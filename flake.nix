fa464995d601f5a94edadb21376a82e14ee3e799
Gunnar Þór Magnússon <gunnarm@tern.is>
2022-10-02 08:16:08 +0000

    Add packages to flake

--- flake.nix
+++ flake.nix
@@ -12,10 +12,23 @@
         pkgs = import nixpkgs { inherit system; };
         tex = pkgs.texlive.combined.scheme-full;
 
+        degenerate = pkgs.callPackage ./degenerate/default.nix { inherit tex; };
+        grassmannian-bundles =
+          pkgs.callPackage ./grassmannian-bundles/default.nix { inherit tex; };
+        journal = pkgs.callPackage ./journal/default.nix { inherit tex; };
+        kodaira-embedding =
+          pkgs.callPackage ./kodaira-embedding/default.nix { inherit tex; };
+        nakano-positive =
+          pkgs.callPackage ./nakano-positive/default.nix { inherit tex; };
         sgct = pkgs.callPackage ./sgct/default.nix { inherit tex; };
       in {
         packages.default = sgct;
 
+        packages.degenerate = degenerate;
+        packages.grassmannian-bundles = grassmannian-bundles;
+        packages.journal = journal;
+        packages.kodaira-embedding = kodaira-embedding;
+        packages.nakano-positive = nakano-positive;
         packages.sgct = sgct;
 
         devShell = pkgs.mkShell { buildInputs = [ tex pkgs.zathura ]; };
